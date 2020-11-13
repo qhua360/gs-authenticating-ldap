fe2c854b2069b774fe7ca810f3e1d72efa6bb332
Qiuyi Hua <qiuyi.hua@capco.com>
2020-11-13 13:43:44 -0500

    Adding Swagger Annotations

--- complete/src/main/java/com/example/authenticatingldap/HomeController.java
+++ complete/src/main/java/com/example/authenticatingldap/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.authenticatingldap;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -7,36 +9,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// Swagger description annotations
 @RestController
 @RequestMapping("/home")
+@Api(value = "welcomehome")
 public class HomeController {
 
     Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/")
+    @ApiOperation(value = "Get home message")
     public String index() {
-    	logger.info("Called index");
+        logger.info("Called index");
         return "Welcome to the home page!";
     }
 
     @GetMapping("/hello")
+    @ApiOperation(value = "Say hello!")
     public String hello()
 //			throws Exception
     {
-    	logger.info("Called hello");
+        logger.info("Called hello");
 //		throw new Exception();
         return "Hello developer!";
     }
 
     @GetMapping("/managers")
+    @ApiOperation(value = "Managerial secrets")
     public String managers() {
-    	logger.info("Called managers");
+        logger.info("Called managers");
         return "Hello manager!";
     }
 
     @GetMapping("/user")
+    @ApiOperation(value = "Get who you are")
     public String user(Authentication authentication) {
-    	logger.info("Called user");
+        logger.info("Called user");
         System.out.println(authentication.getAuthorities());
         return authentication.getName();
     }
